077acd1ca82719315e27f9df7ae8b34cd6877c8d
Tiago Silva <tiagobsb84@gmail.com>
2024-09-18 12:53:35 -0300

    criando metodo para atualizando a viagem.

--- backend/src/main/java/com/tiago/planning/trip/ControllerTrip.java
+++ backend/src/main/java/com/tiago/planning/trip/ControllerTrip.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -34,5 +36,23 @@ public class ControllerTrip {
         Optional<Trip> trip = repositoryTrip.findById(id);
 
         return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable UUID id, @RequestBody TripRequestLoad obj) {
+        Optional<Trip> trip = repositoryTrip.findById(id);
+
+        if(trip.isPresent()) {
+            Trip rowTrip = trip.get();
+            rowTrip.setDestination(obj.destination());
+            rowTrip.setStartAs(LocalDateTime.parse(obj.start_as(), DateTimeFormatter.ISO_DATE_TIME));
+            rowTrip.setEndAs(LocalDateTime.parse(obj.end_as(), DateTimeFormatter.ISO_DATE_TIME));
+
+            this.repositoryTrip.save(rowTrip);
+
+            return ResponseEntity.ok(rowTrip);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
